5f3887c224e727276a3bce0c8c60d3e56fb8cc7d
HemantKarekar <hemant.v.karekar@gmail.com>
2021-07-14 23:34:23 +0530

    Edit App.js

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -5,14 +5,12 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      {/* Tinder Cards */ } 
+      {/* Swipe Buttons Cards */ } 
+      {/* Chat Screen */ } 
+      {/* Individual Chat Screen */ } 
       <h1>Let's Build React App !</h1>
     </div>
-    
-    /* Header */
-    /* Tinder Cards */
-    /* Swipe Buttons Cards */
-    /* Chat Screen */
-    /* Individual Chat Screen */
 
   );
 }
--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,18 +1,11 @@
 //import './Header.css';
 
 function Header() {
-  return (
-    <div className="App">
-      <h1>Let's Build Header !</h1>
-    </div>
-     
-    /* Header */
-    /* Tinder Cards */
-    /* Swipe Buttons Cards */
-    /* Chat Screen */
-    /* Individual Chat Screen */
-
-  );
+    return ( <div className = "App" >
+        {/* Header */ } 
+        <h1> Let 's Build Header !</h1> 
+        </div>
+    );
 }
 
 export default Header;
