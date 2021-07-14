33143c704f43a78e28343e24dc196ca24510844b
HemantKarekar <hemant.v.karekar@gmail.com>
2021-07-14 23:05:09 +0530

    Tinder Clone Added

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lest Build React App :rocket:</h1>
+    </div>
+  );
+}
+
+export default App;
