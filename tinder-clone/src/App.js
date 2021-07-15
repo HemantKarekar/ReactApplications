f1298c6b00f0f98377167878efb7886796fc677a
HemantKarekar <hemant.v.karekar@gmail.com>
2021-07-15 15:53:41 +0530

    Edit on SASS Extension Adding CSS files

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/css/style.css';
 import Header from './Header';
 
 function App() {
--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,10 +1,11 @@
 //import './Header.css';
 
 function Header() {
-    return ( <div className = "App" >
+    return ( 
+    <div className = "Header" >
         {/* Header */ } 
         <h1> Let 's Build Header !</h1> 
-        </div>
+    </div>
     );
 }
 
